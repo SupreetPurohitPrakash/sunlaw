39a87be44977aabb917c3166cd3a914bfa1cccc2
Supreet <supreet.official2020@gmail.com>
2021-02-13 02:17:35 +0545

    all pages finished || only responsive left

--- js/custom.js
+++ js/custom.js
@@ -111,7 +111,7 @@ jQuery(document).ready(function ($) {
     // offset_top: 100,
     // offset_bottom: 100
   });
-  $(".serv-side").stick_in_parent({
+  $(".side-sticky").stick_in_parent({
     offset_top: 120,
     // offset_bottom: 100
   });
